c205ab0f580db53caa758f56ba5a389a8c775b3f
josemendoza <prueba@correo.com>
2025-04-29 18:24:36 -0400

    Añadiendo mejoras de navegación

--- app/components/Details/DetailsHeader.tsx
+++ app/components/Details/DetailsHeader.tsx
@@ -11,11 +11,13 @@ const DetailsHeader: React.FC<DetailsHeaderProps> = ({ onSearch }) => {
   <nav className="flex md:h-[100px] md:justify-center md:bg-[#98c1d9] ">
    <div className="flex h-[85px] w-full items-center justify-evenly md:my-auto md:flex  md:h-[80px] md:w-[1070px] md:items-start md:justify-normal">
     <div className="md:block md:h-[80px] md:w-[120px]">
-     <BazarIcon
-      className="md:mx-auto md:mt-[8px] md:h-[50px] md:w-[50px]"
-      height={80}
-      width={65}
-     />
+     <a href="/">
+      <BazarIcon
+       className="md:mx-auto md:mt-[8px] md:h-[50px] md:w-[50px]"
+       height={80}
+       width={65}
+      />
+     </a>
      <p className="hidden  md:block md:h-[24px] md:w-[120px] md:text-nowrap md:text-center md:text-[14.85px] md:font-extrabold md:leading-[24px]">
       Pocket Market
      </p>
